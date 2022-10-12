6ac86e2750cee27e82eef1c3b7b377dbe615ed65
Anastasiya Z <nastya_tim2201@mail.ru>
2022-10-12 16:42:26 +0300

    изучили ветвление

---
+++ Example5_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hello, darling!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
